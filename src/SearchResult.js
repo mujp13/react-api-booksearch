464317932e054176793d439e1314b8560c11801b
Kevin Park <keunsuk413@gmail.com>
2020-02-06 22:05:16 -0600

    css done...finalized project

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -12,12 +12,16 @@ export default class SearchResult extends React.Component {
     return (
       <div className='book'>
         <div className='title'>
-          <p>{this.props.Title}</p>
+          <p><b>{this.props.Title}</b></p>
         </div>
-        <div className='author'>{this.props.Author}</div>
-        <div className='price'>{USCurrencyFormat.format(this.props.Price)}</div>
-        <div className='desc'>{this.props.Desc}</div>
-        <img src={this.props.Image} alt='Bookpic' />
+        <div className='within-search'>
+          <div className='texts'>
+            <div className='author'>Author: {this.props.Author}</div>
+            <div className='price'>Price: {USCurrencyFormat.format(this.props.Price)}</div>
+            <div className='desc'>{this.props.Desc}</div>
+          </div>
+          <img className='image' src={this.props.Image} alt='Bookpic' />
+        </div>
       </div>
     );
   }
